bef4270efab9e5cd1be5dbc85c12ee0b8fa323d6
hungHoangDang <33661599+hungHoangDang@users.noreply.github.com>
2018-01-16 11:41:30 +0700

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+const ChallengeApi = [
+  {
+    name: "Hang out with your friends",
+    text:
+      "Pick up the phone and call your bestfriends. It is hightime for you to  confide your feelings to some good listener."
+  },
+  {
+    name: "Have a pet",
+    text:
+      "A dog or a cat can be a good company when you are lonely. Taking care of the pet helps you to put your mind into something and distract yourself from your suffering."
+  },
+  {
+    name: "Travel to a distant place",
+    text:
+      "Being close to natural beauty can be a great way to heal your soul. On your journey, who know what wonderful adventures are awaiting you."
+  }
+];
+
+export default ChallengeApi;
