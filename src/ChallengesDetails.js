64208927313e5180a01fd73166b496dfd461e14c
hungHoangDang <33661599+hungHoangDang@users.noreply.github.com>
2018-01-18 16:41:06 +0700

    Make the text appear for Challenge

--- src/ChallengesDetails.js
+++ src/ChallengesDetails.js
@@ -5,20 +5,30 @@ export default class ChallengesDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      textValue: ""
+      textValue: props.ChallengeApi.name
     };
   }
+
+  /*
+  handleClick() {
+    () => {
+      e.preventDefault()
+      this.setState({
+        isChallengeXVisible: true,
+        textValue: this.props.ChallengeApi.text
+      })
+    }
+  }
+  */
 
   render() {
     return (
       <div>
         <a
           href="#"
-          onClick={() =>
-            this.setState({ textValue: this.props.ChallengeApi.text })
-          }
+          onClick={() => this.setState({textValue: this.props.ChallengeApi.text})}
         >
-          {this.props.ChallengeApi.name}
+          {this.state.textValue}
         </a>
       </div>
     );
